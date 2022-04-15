5c9637301019810db7549a2a74597986750ce224
Vedanth Bora <vedanthbora@gmail.com>
2022-04-16 01:58:26 +0530

    [FEAT]: mapped the playlists in playlist list

--- src/frontend/components/explore-page/PlaylistListItem.js
+++ src/frontend/components/explore-page/PlaylistListItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function PlaylistListItem({ playlistName, value, onChange }) {
+function PlaylistListItem({ playlistName }) {
   return (
     <li className="pb-3">
       <label className="text-black text-lg">
         <input
           type="checkbox"
-          value={value}
-          onChange={onChange}
+          // value={value}
+          // onChange={onChange}
           className="mr-3"
         />
         {playlistName}
